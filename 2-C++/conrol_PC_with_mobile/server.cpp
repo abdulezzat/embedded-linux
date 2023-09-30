bb3fc9ebb51f2908fb0c3dd49bb5b185f7f912bd
AbdullahEzzat <41158260+abdulezzat@users.noreply.github.com>
2023-09-30 20:15:56 +0300

    add client

--- 2-C++/conrol_PC_with_mobile/server.cpp
+++ 2-C++/conrol_PC_with_mobile/server.cpp
@@ -1,64 +1,40 @@
-// Server side C/C++ program to demonstrate Socket
-// programming
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <iostream>
+#include <cstring>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <unistd.h>
-#define PORT 8080
-int main(int argc, char const* argv[])
-{
-	int server_fd, new_socket, valread;
-	struct sockaddr_in address;
-	int opt = 1;
-	int addrlen = sizeof(address);
-	char buffer[1024] = { 0 };
-	char* hello = "Hello from server";
 
-	// Creating socket file descriptor
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		perror("socket failed");
-		exit(EXIT_FAILURE);
-	}
+int main() {
+    int server_socket, client_socket;
+    sockaddr_in server_address, client_address;
+    socklen_t client_length;
 
-	// Forcefully attaching socket to the port 8080
-	if (setsockopt(server_fd, SOL_SOCKET,
-				SO_REUSEADDR | SO_REUSEPORT, &opt,
-				sizeof(opt))) {
-		perror("setsockopt");
-		exit(EXIT_FAILURE);
-	}
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(PORT);
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
-	// Forcefully attaching socket to the port 8080
-	if (bind(server_fd, (struct sockaddr*)&address,
-			sizeof(address))
-		< 0) {
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
-	if (listen(server_fd, 3) < 0) {
-		perror("listen");
-		exit(EXIT_FAILURE);
-	}
-	if ((new_socket
-		= accept(server_fd, (struct sockaddr*)&address,
-				(socklen_t*)&addrlen))
-		< 0) {
-		perror("accept");
-		exit(EXIT_FAILURE);
-	}
-	valread = read(new_socket, buffer, 1024);
-	printf("%s\n", buffer);
-	send(new_socket, hello, strlen(hello), 0);
-	printf("Hello message sent\n");
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(8000);
+    // server_address.sin_addr.s_addr = INADDR_ANY;
+	server_address.sin_addr.s_addr = inet_addr("192.168.221.40");
 
-	// closing the connected socket
-	close(new_socket);
-	// closing the listening socket
-	shutdown(server_fd, SHUT_RDWR);
-	return 0;
+    bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address));
+    listen(server_socket, 5);
+
+    std::cout << "Server is listening on port 8000..." << std::endl;
+
+    client_length = sizeof(client_address);
+    client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_length);
+
+	std::cout << "Server accept ..." << std::endl;
+
+    char buffer[1024] = {0};
+    read(client_socket, buffer, sizeof(buffer));
+    std::cout << "Received: " << buffer << std::endl;
+
+    send(client_socket, buffer, strlen(buffer), 0);
+
+    close(client_socket);
+    close(server_socket);
+
+    return 0;
 }
